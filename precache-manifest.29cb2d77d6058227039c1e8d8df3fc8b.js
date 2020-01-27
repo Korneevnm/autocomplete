e2f859d3ee7ab4f359e70a6db75ea1b1df079251
Korneevnm <Korneevnm@yandex.ru>
2020-01-27 14:21:32 +0400

    Updates

---
+++ precache-manifest.29cb2d77d6058227039c1e8d8df3fc8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71867bdc1b53a896abecc81bbe28e568",
+    "url": "/autocomplete/index.html"
+  },
+  {
+    "revision": "c1a0697a3876e776b5bb",
+    "url": "/autocomplete/static/css/2.c726b981.chunk.css"
+  },
+  {
+    "revision": "2e21e8e3a73a5aa0e185",
+    "url": "/autocomplete/static/css/main.23717a9c.chunk.css"
+  },
+  {
+    "revision": "c1a0697a3876e776b5bb",
+    "url": "/autocomplete/static/js/2.8bfeadc8.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/autocomplete/static/js/2.8bfeadc8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e21e8e3a73a5aa0e185",
+    "url": "/autocomplete/static/js/main.a88037c5.chunk.js"
+  },
+  {
+    "revision": "33ad8a3cede398ac0b4e",
+    "url": "/autocomplete/static/js/runtime-main.e8ad3709.js"
+  }
+]);
